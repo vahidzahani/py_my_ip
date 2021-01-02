c1725424730ec6c54b352c1129ee362cb47bba82
vahid zahani <vahid.zahani@gmail.com>
2021-01-02 11:04:08 +0330

    add py kernel

---
+++ getip.py
@@ -0,0 +1,29 @@
+#this is edit in drpbox mobile
+import subprocess
+import urllib.request
+import time,json
+
+def getIp():
+    data=urllib.request.urlopen("https://api.projfa.ir/ip").read().decode('utf-8')
+    data=json.loads(data)
+    return(data['ip'])
+
+lastip="NOIP"
+while True:
+    try:
+        newip=getIp()
+        if (lastip!=newip):
+            # with open("d:\\aaa.txt", "w") as f:
+            #     f.write(lastip +" -> "+ newip +' IP Change!\n')
+            #subprocess.Popen('notepad "d:\\aaa.txt"')
+            lastip=newip
+            print("changed IP")
+        print(newip)
+    except :
+        print("Error to connect")
+    time.sleep(4)
+
+
+
+
+    
